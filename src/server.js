593f6ae7aba0866aa677fcaa36146b78080806fd
Juho Choi <juho.c98@gmail.com>
2022-09-27 18:21:39 -0400

    .

--- src/server.js
+++ src/server.js
@@ -1,9 +1,32 @@
 import express from "express";
-
-const PORT = 4000;
+import morgan from "morgan";
+import session from "express-session";
+import MongoStore from "connect-mongo";
+import rootRouter from "./routers/rootRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import { localsMiddleware } from "./middlewares";
 
 const app = express();
+const logger = morgan("dev");
 
-const handleListening = () => console.log(`Server listening on port {} 🚀`);
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
+app.use(logger);
+app.use(express.urlencoded({extended: true}));
 
-app.listen(4000, handleListening);
+app.use(session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.DB_URL })
+}));
+
+app.use(localsMiddleware);
+app.use("/uploads", express.static("uploads"));
+app.use("/assets", express.static("assets"));
+app.use("/", rootRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
+
+export default app;
