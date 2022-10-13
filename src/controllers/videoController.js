38f86a6fcf8c313af7d69269e5742f426c161da5
Juho Choi <juho.c98@gmail.com>
2022-10-13 16:52:52 -0400

    Build basic HTML structure using Pug

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,9 +1,46 @@
-export const trending = (req, res) => res.send("Home Page Videos");
-export const see = (req, res) => {
-    console.log(req.params);
-    return res.send("Watch");
+let videos = [
+    {
+        title: "Video #1",
+        rating: 5,
+        comments: 2,
+        createdAt: "2 minutes ago",
+        views: 1,
+        id:1
+    },
+    {
+        title: "Video #2",
+        rating: 5,
+        comments: 2,
+        createdAt: "2 minutes ago",
+        views: 59,
+        id:2
+    },
+    {
+        title: "Video #3",
+        rating: 5,
+        comments: 2,
+        createdAt: "2 minutes ago",
+        views: 59,
+        id:3
+    },
+];
+
+export const trending = (req, res) => {
+    return res.render("home", { pageTitle: "Home", videos });
 };
-export const edit = (req, res) => res.send("Edit");
-export const search = (req, res) => res.send("Search");
-export const deleteVideo = (req, res) => res.send("Delete Video");
-export const upload = (req, res) => res.send("Upload");
+export const watch = (req, res) => {
+    const { id } = req.params;
+    const video = videos[id-1];
+    return res.render("watch", { pageTitle: `Watching ${video.title}`, video }); 
+}
+export const getEdit = (req, res) => {
+    const { id } = req.params;
+    const video = videos[id-1];
+    return res.render("edit", { pageTitle: `Editing ${video.title}`, video });
+};
+export const postEdit = (req, res) => {
+    const { id } = req.params;
+    const { title } = req.body;
+    videos[id-1].title = title;
+    return res.redirect(`/videos/${id}`);
+};
